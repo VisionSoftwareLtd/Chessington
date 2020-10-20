74967b72bd368585712f683bae9c9ed84cf99f2a
Dave Wright <dave@visionsoftware.uk>
2020-10-20 10:38:11 +0100

    Added red-2 functionality

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,6 +15,36 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        PlayerColour myColour;
+        List<Move> moves;
+        Coordinates to;
+        Move moveOne;
+        Move moveTwo;
+        int row;
+
+        myColour = getColour();
+        row = from.getRow();
+        moves = new ArrayList<>();
+        if (myColour == PlayerColour.BLACK) {
+            to = from.plus(1, 0);
+        } else {
+            to = from.plus(-1,0);
+        }
+        moveOne = new Move(from, to);
+
+        moveTwo = null;
+        if (myColour == PlayerColour.BLACK && row == 1) {
+            to = from.plus(2, 0);
+            moveTwo = new Move(from, to);
+        } else if (myColour == PlayerColour.WHITE && row == 6){
+            to = from.plus(-2,0);
+            moveTwo = new Move(from, to);
+        }
+
+        moves.add(moveOne);
+        if (moveTwo != null) {
+            moves.add(moveTwo);
+        }
+        return moves;
     }
 }
